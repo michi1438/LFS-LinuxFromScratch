0be4c20f0f8af766200ac2edc3efb2af25fcd09c
michael HOMESERVER <michaelguerga@gmail.com>
2024-11-22 06:01:59 +0100

    .env_layout for referential purpuses

--- limited_dir_layout.sh
+++ limited_dir_layout.sh
@@ -17,3 +17,8 @@ mkdir -pv $LFS/tools
 #Create new user lfs and group lfs in the aim of not f**ng up my computer
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+
+chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
+case $(uname -m) in
+  x86_64) chown -v lfs $LFS/lib64 ;;
+esac
