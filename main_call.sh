47d16cd504d0d9d78bcce59579827fde03eabe45
michael HOMESERVER <michaelguerga@gmail.com>
2024-12-09 11:42:08 +0100

    entered chroot in $/mnt/lfs

--- main_call.sh
+++ main_call.sh
@@ -36,6 +36,45 @@ then
 call bash main_call.sh this will run the rest of the script...${NC}\n"
 	su --login lfs
 
+	for dir in $LFS/sources/{sed-4.9/,tar-1.35/,xz-5.6.2/,file-5.45/,grep-3.11/,gzip-1.13/,m4-1.4.19/,gawk-5.3.0/,gcc-14.2.0/,glibc-2.40/,make-4.4.1/,bash-5.2.32/,ncurses-6.5/,patch-2.7.6/,linux-6.10.5/,coreutils-9.5/,diffutils-3.10/}
+	do
+		if [ ! -d "$dir" ]; then
+			printf "${RED}MAIN_CALL.SH: Chapter 6 was not finished all the \
+required directories are not there, $dir is missing...${NC}\n"
+			exit
+		fi
+		printf "${RED}MAIN_CALL.SH: dir $dir exists!${NC}\n"
+	done
+
+	printf "${RED}MAIN_CALL.SH: Chapter 7 -- Entering Chroot.${NC}\n"
+	chown --from lfs -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
+	case $(uname -m) in
+	  x86_64) chown --from lfs -R root:root $LFS/lib64 ;;
+	esac
+	
+	printf "${RED}MAIN_CALL.SH: Creating virtual fs!${NC}\n"
+	mkdir -pv $LFS/{dev,proc,sys,run}
+	mount -v --bind /dev $LFS/dev
+	mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts
+	mount -vt proc proc $LFS/proc
+	mount -vt sysfs sysfs $LFS/sys
+	mount -vt tmpfs tmpfs $LFS/run
+
+	if [ -h $LFS/dev/shm ]; then
+		install -v -d -m 1777 $LFS$(realpath /dev/shm)
+	else
+		mount -vt tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
+	fi
+
+	chroot "$LFS" /usr/bin/env -i   \
+		HOME=/root                  \
+		TERM="$TERM"                \
+		PS1='(lfs chroot) \u:\w\$ ' \
+		PATH=/usr/bin:/usr/sbin     \
+		MAKEFLAGS="-j$(nproc)"      \
+		TESTSUITEFLAGS="-j$(nproc)" \
+		/bin/bash --login
+
 	[ ! -e /etc/bash.bashrc.NOUSE ] || mv -v /etc/bash.bashrc.NOUSE /etc/bash.bashrc
 	exit
 elif [ "$USER" = "lfs" ]
@@ -69,6 +108,8 @@ then
 	bash ${PART_3_DIR}6.16_Xz-5.6.2.sh
 	bash ${PART_3_DIR}6.17_Binutils-2.43.1_pass_2.sh
 	bash ${PART_3_DIR}6.18_Gcc-14.2.0_pass_2.sh
+	printf "${RED}MAIN_CALL_LFS.SH: You need to switch to root, \
+exit to return to root, the rest of the script should execute on its own...${NC}\n"
 
 fi
 
